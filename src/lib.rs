b4a16e6f8b037e50c7266d02bbeccddaac8fbc33
Saurabh Singh <saurabhsingh.rwx@gmail.com>
2021-06-24 00:21:26 +0530

    #16 Add delay functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![deny(warnings)]
 
+//! Rustduino library for arduino
+
 /// Library for ATmega328P chip.
 pub mod atmega328p {
     /// Hardware Abstraction Library (HAL).
@@ -15,10 +17,18 @@ pub mod atmega328p {
     }
 }
 
+/// delay for N miliseconds
+///
+/// ## Arguments
+/// * 'ms' - an u32, number of milliseconds to busy-wait
 pub fn delay_ms(ms: u32) {
     avr_delay::delay_ms(ms);
 }
 
+/// delay for N microseconds
+///
+/// ## Arguments
+/// * 'us' - an u32, number of microseconds to busy-wait
 pub fn delay_us(us: u32) {
     avr_delay::delay_us(us);
 }
