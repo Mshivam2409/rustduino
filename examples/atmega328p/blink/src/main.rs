2c148a83a43e4fbab65143c64ab3005533535c72
Saurabh Singh <saurabhsingh.rwx@gmail.com>
2021-06-24 00:21:51 +0530

    #16 Update LED blink example

--- examples/atmega328p/blink/src/main.rs
+++ examples/atmega328p/blink/src/main.rs
@@ -1,17 +1,32 @@
+#![no_std]
+#![no_main]
+#![deny(warnings)]
+
 use rustduino::atmega328p::hal as arduino_uno;
 
-fn main() {
-    let pins = arduino_uno::pins::Pins::get();
+#[no_mangle]
+pub extern "C" fn main() {
+    // Disable watchdog
+    let wdog = arduino_uno::watchdog::Watchdog::new();
+    wdog.disable();
+
+    let mut pins = arduino_uno::pins::Pins::get();
 
     pins.digital[13].set_output();
 
     loop {
         pins.digital[13].high();
 
-        avr_delay::delay_ms(1000);
+        rustduino::delay_ms(1000);
 
         pins.digital[13].low();
 
-        avr_delay::delay_ms(1000);
+        rustduino::delay_ms(1000);
     }
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
