542716b72e2d6d4daecdf112bb0e2238260e34aa
Devansh Kumar Jha <77242838+dkjha20@users.noreply.github.com>
2021-06-24 12:54:46 +0530

    #17 #18 changes in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+/*
 #![no_std]
 
 #[cfg(test)]
@@ -5,5 +6,20 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+}
+*/
+
+#![no_std]
+#![deny(warnings)]
+
+/// Library for ATmega2560P chip.
+pub mod atmega2560p {
+    /// Hardware Abstraction Library (HAL).
+    pub mod hal {
+        pub mod watchdog;
+        pub mod sleep_mode;
+        pub mod port;
+        pub mod interrupt;
     }
 }
