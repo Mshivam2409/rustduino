6389ad631d528e2821e9b8b7256a50bc8f4bc2b9
Kshitij <kshitijk20@iitk.ac.in>
2021-06-24 22:37:44 +0530

    #16, added comments

--- examples/atmega328p/blink/src/main.rs
+++ examples/atmega328p/blink/src/main.rs
@@ -6,14 +6,14 @@ use rustduino::atmega328p::hal as arduino_uno;
 
 #[no_mangle]
 pub extern "C" fn main() {
-    // Disable watchdog
+    // Disable the watchdog
     let wdog = arduino_uno::watchdog::Watchdog::new();
     wdog.disable();
-
+    // Get a mutable reference to the Pins struct
     let mut pins = arduino_uno::pins::Pins::get();
-
+    //Set the digital pin 13 as an output pin.
     pins.digital[13].set_output();
-
+    // Iterative function to make the LED blink.
     loop {
         pins.digital[13].high();
 
