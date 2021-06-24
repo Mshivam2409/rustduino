88b8f35c01bb0d956e43a54106379fce7068af8e
sanmatip20 <84724879+sanmatip20@users.noreply.github.com>
2021-06-24 21:50:23 +0530

    added comments to gating.rs

--- src/atmega328p/hal/gating.rs
+++ src/atmega328p/hal/gating.rs
@@ -1,6 +1,7 @@
+////// Power reduction for ATmega328p chip
 pub enum Peripherals {
-    TWI,
-    Timer2,
+    TWI, 
+    Timer2, 
     Timer0,
     Timer1,
     SPI, //serial peripheral interface
@@ -9,6 +10,7 @@ pub enum Peripherals {
 }
 ///registers controlling power management
 pub struct Power {
+    //power reduction register control bits for power management.
     prr: u8,
 }
 ///returns mutable reference to Power struct
@@ -16,10 +18,12 @@ impl Power {
     pub fn get() -> &'static mut Self {
         unsafe { &mut *(0x64 as *mut Self) }
     }
+    
     pub fn twi(&mut self) {
         unsafe {
             let mut ctrl_twi = core::ptr::read_volatile(&mut self.prr);
             ctrl_twi |= 0x80;
+            //setting 7th bit shuts down the TWI by stopping the clock to the module.
             core::ptr::write_volatile(&mut self.prr, ctrl_twi);
         }
     }
@@ -27,6 +31,7 @@ impl Power {
         unsafe {
             let mut ctrl_timer2 = core::ptr::read_volatile(&mut self.prr);
             ctrl_timer2 |= 0x40;
+            //setting 6th bit shuts down the Timer/Counter2 module in synchronous mode 
             core::ptr::write_volatile(&mut self.prr, ctrl_timer2);
         }
     }
@@ -34,6 +39,7 @@ impl Power {
         unsafe {
             let mut ctrl_timer0 = core::ptr::read_volatile(&mut self.prr);
             ctrl_timer0 |= 0x20;
+            //setting 5th bit shuts down the Timer/Counter0 module.
             core::ptr::write_volatile(&mut self.prr, ctrl_timer0);
         }
     }
@@ -41,6 +47,7 @@ impl Power {
         unsafe {
             let mut ctrl_timer1 = core::ptr::read_volatile(&mut self.prr);
             ctrl_timer1 |= 0x8;
+            //setting 3rd bit shuts down the Timer/Counter1 module.
             core::ptr::write_volatile(&mut self.prr, ctrl_timer1);
         }
     }
@@ -48,6 +55,7 @@ impl Power {
         unsafe {
             let mut ctrl_spi = core::ptr::read_volatile(&mut self.prr);
             ctrl_spi |= 0x4;
+            //setting 2nd bit shuts down the serial peripheral interface by stopping the clock to the module.
             core::ptr::write_volatile(&mut self.prr, ctrl_spi);
         }
     }
@@ -55,6 +63,7 @@ impl Power {
         unsafe {
             let mut ctrl_usart0 = core::ptr::read_volatile(&mut self.prr);
             ctrl_usart0 |= 0x2;
+            //setting 1st bit shuts down the USART by stopping the clock to the module
             core::ptr::write_volatile(&mut self.prr, ctrl_usart0);
         }
     }
@@ -62,10 +71,11 @@ impl Power {
         unsafe {
             let mut ctrl_adc = core::ptr::read_volatile(&mut self.prr);
             ctrl_adc |= 0x1;
+            //setting 0th bit shuts down the ADC
             core::ptr::write_volatile(&mut self.prr, ctrl_adc);
         }
     }
-
+ ///disable_clock function disables the mode as per requirement
     pub fn disable_clock(mode: Peripherals) {
         match mode {
             Peripherals::TWI => Power::twi(&mut Power::get()),
