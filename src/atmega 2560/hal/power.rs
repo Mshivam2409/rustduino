9d123ba5aa117893c3eb679d311a6925145dd38f
Devansh <devansh.kujha2003@gmail.com>
2021-06-25 10:15:09 +0530

    #18 #17 changes in power and sleep

--- src/atmega 2560/hal/power.rs
+++ src/atmega 2560/hal/power.rs
@@ -48,6 +48,57 @@ impl Power {
         &mut *(0x64 as *mut Power)
     }
 
+    pub fn enable(&mut self,mode:Options) {
+        unsafe {
+            // Here we would create a new element of the structure Power
+            // and it would be used to control various clock gating features of the
+            // chip ATMEGA2560P
+
+            Power *ptr = new();
+            let mut prr = 0;
+            match mode {
+                {  Options::TWI    || Options::TIMER2 ||
+                   Options::TIMER0 || Options::TIMER1 ||
+                   Options::SPI    || Options::USART0 || 
+                   Options::ADC                              }     => prr = core::ptr::read_volatile(&mut self.PRR0),
+
+                {  Options::TIMER5    || Options::TIMER4 ||
+                   Options::TIMER3    || Options::USART3 ||
+                   Options::USART2    || Options::USART1     }     => prr = core::ptr::read_volatile(&mut self.PRR1); 
+
+            }
+
+            match mode {
+                Options::TWI => prr = prr | 0x80,
+                Options::TIMER2 => prr = prr | 0x40,
+                Options::TIMER0 => prr = prr | 0x20,
+                Options::TIMER1 => prr = prr | 0x08,
+                Options::SPI => prr = prr | 0x04,
+                Options::USART0 => prr = prr | 0x02,
+                Options::ADC => prr = prr | 0x01,
+                Options::TIMER5 => prr = prr | 0x20,
+                Options::TIMER4 => prr = prr | 0x10,
+                Options::TIMER3 => prr = prr | 0x08,
+                Options::USART3 => prr = prr | 0x04,
+                Options::USART2 => prr = prr | 0x02,
+                Options::USART1 => prr = prr | 0x01
+            }
+
+            match mode {
+                {  Options::TWI    || Options::TIMER2 ||
+                   Options::TIMER0 || Options::TIMER1 ||
+                   Options::SPI    || Options::USART0 || 
+                   Options::ADC                              }     => prr = core::ptr::write_volatile(&mut self.PRR0,prr),
+
+                {  Options::TIMER5    || Options::TIMER4 ||
+                   Options::TIMER3    || Options::USART3 ||
+                   Options::USART2    || Options::USART1     }     => prr = core::ptr::write_volatile(&mut self.PRR1,prr); 
+
+            }
+        }
+    }
+    
+    /*
     pub fn enable_twi(&mut self) {
         unsafe {
             // A instance to the PRR0 register is created and the corresponding bit is controlled
@@ -168,8 +219,59 @@ impl Power {
             core::ptr::write_volatile(&mut self.PRR1, usart1);
         }
     }
+    */
 
+    pub fn disable(&mut self,mode:Options) {
+        unsafe {
+            // Here we would create a new element of the structure Power
+            // and it would be used to control various clock gating features of the
+            // chip ATMEGA2560P
+            
+            Power *ptr = new();
+            let mut prr = 0;
+            match mode {
+                {  Options::TWI    || Options::TIMER2 ||
+                   Options::TIMER0 || Options::TIMER1 ||
+                   Options::SPI    || Options::USART0 || 
+                   Options::ADC                              }     => prr = core::ptr::read_volatile(&mut self.PRR0),
 
+                {  Options::TIMER5    || Options::TIMER4 ||
+                   Options::TIMER3    || Options::USART3 ||
+                   Options::USART2    || Options::USART1     }     => prr = core::ptr::read_volatile(&mut self.PRR1); 
+
+            }
+
+            match mode {
+                Options::TWI => prr = prr & 0x7F,
+                Options::TIMER2 => prr = prr & 0xBF,
+                Options::TIMER0 => prr = prr & 0xDF,
+                Options::TIMER1 => prr = prr & 0xF7,
+                Options::SPI => prr = prr & 0xFB,
+                Options::USART0 => prr = prr & 0xFD,
+                Options::ADC => prr = prr & 0xFE,
+                Options::TIMER5 => prr = prr & 0xDF,
+                Options::TIMER4 => prr = prr & 0xEF,
+                Options::TIMER3 => prr = prr & 0xF7,
+                Options::USART3 => prr = prr & 0xFB,
+                Options::USART2 => prr = prr & 0xFD,
+                Options::USART1 => prr = prr & 0xFE
+            }
+
+            match mode {
+                {  Options::TWI    || Options::TIMER2 ||
+                   Options::TIMER0 || Options::TIMER1 ||
+                   Options::SPI    || Options::USART0 || 
+                   Options::ADC                              }     => prr = core::ptr::write_volatile(&mut self.PRR0,prr),
+
+                {  Options::TIMER5    || Options::TIMER4 ||
+                   Options::TIMER3    || Options::USART3 ||
+                   Options::USART2    || Options::USART1     }     => prr = core::ptr::write_volatile(&mut self.PRR1,prr); 
+
+            }
+        }
+    }
+
+    /*
     pub fn disable_twi(&mut self) {
         unsafe {
             // A instance to the PRR0 register is created and the corresponding bit is controlled
@@ -290,54 +392,55 @@ impl Power {
             core::ptr::write_volatile(&mut self.PRR1, usart1);
         }
     }
+    */
 
 
     // These are the functions directly to be controlled by the user using calls in the main function
     // We will provide seperate functions for enabling "power-down" and "power-up" features
 
     // This is the function for disabling the clock system of your choice
-    pub fn disable_clockgates(&mut self,mode: Options) {
+    pub fn disable_clocks(&mut self,mode: Options) {
         // Creates a new instance to the power structure
         Power *power = new();
         match mode {
-            Options::TWI => power.enable_twi(),
-            Options::TIMER2 => power.enable_timer2(),
-            Options::TIMER0 => power.enable_timer0(),
-            Options::TIMER1 => power.enable_timer1(),
-            Options::SPI => power.enable_spi(),
-            Options::USART0 => power.enable_usart0(),
-            Options::ADC => power.enable_adc(),
-            Options::TIMER5 => power.enable_timer5(),
-            Options::TIMER4 => power.enable_timer4(),
-            Options::TIMER3 => power.enable_timer3(),
-            Options::USART3 => power.enable_usart3(),
-            Options::USART2 => power.enable_usart2(),
-            Options::USART1 => power.enable_usart1(),
-            _ => unreachable!();
+            Options::TWI => power.enable(mode),
+            Options::TIMER2 => power.enable(mode),
+            Options::TIMER0 => power.enable(mode),
+            Options::TIMER1 => power.enable(mode),
+            Options::SPI => power.enable(mode),
+            Options::USART0 => power.enable(mode),
+            Options::ADC => power.enable(mode),
+            Options::TIMER5 => power.enable(mode),
+            Options::TIMER4 => power.enable(mode),
+            Options::TIMER3 => power.enable(mode),
+            Options::USART3 => power.enable(mode),
+            Options::USART2 => power.enable(mode),
+            Options::USART1 => power.enable(mode),
+            _ => unreachable!()
         }
     }
 
     // This is the function for enabling back the clock system of your choice
     // User has to make sure that these functions are called after disabling functions
     // otherwise they have no effect.
-    pub fn enable_clockgates(&mut self,mode: Options) {
+    pub fn enable_clocks(&mut self,mode: Options) {
         // Creates a new instance to the power structure
         Power *power = new();
         match mode {
-            Options::TWI => power.disable_twi(),
-            Options::TIMER2 => power.disable_timer2(),
-            Options::TIMER0 => power.disable_timer0(),
-            Options::TIMER1 => power.disable_timer1(),
-            Options::SPI => power.disable_spi(),
-            Options::USART0 => power.disable_usart0(),
-            Options::ADC => power.disable_adc(),
-            Options::TIMER5 => power.disable_timer5(),
-            Options::TIMER4 => power.disable_timer4(),
-            Options::TIMER3 => power.disable_timer3(),
-            Options::USART3 => power.disable_usart3(),
-            Options::USART2 => power.disable_usart2(),
-            Options::USART1 => power.disable_usart1(),
-            _ => unreachable!();
+            Options::TWI => power.disable(mode),
+            Options::TIMER2 => power.disable(mode),
+            Options::TIMER0 => power.disable(mode),
+            Options::TIMER1 => power.disable(mode),
+            Options::SPI => power.disable(mode),
+            Options::USART0 => power.disable(mode),
+            Options::ADC => power.disable(mode),
+            Options::TIMER5 => power.disable(mode),
+            Options::TIMER4 => power.disable(mode),
+            Options::TIMER3 => power.disable(mode),
+            Options::USART3 => power.disable(mode),
+            Options::USART2 => power.disable(mode),
+            Options::USART1 => power.disable(mode),
+            _ => unreachable!()
         }
     }
     
