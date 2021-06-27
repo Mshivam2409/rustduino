5010124a6e283183f46d0cd207c270f014c191c7
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2021-06-27 14:01:43 +0530

    #17 added some functions

--- src/atmega2560p/hal/port.rs
+++ src/atmega2560p/hal/port.rs
@@ -1,6 +1,7 @@
-use core;
+use core::ptr::{read_volatile, write_volatile};
  
 #[derive(Clone,Copy)]
+/// Represents the name of the port , can vary from A-L.
 pub enum PortName {
     A,
     B,
@@ -14,6 +15,35 @@ pub enum PortName {
     K,
     L,
 }
+
+///Each Port pin consists of 3 registers : PINx, DDRx, PORTx .
+///These registors control the ports.
+
+
+///Registers and bit references used here are written in general form.
+///A lower case “x” represents the numbering letter for the port, and a lower case “n” represents the bit number.
+///For example, PORTB3 for bit no. 3 in Port B, here documented generally as PORTxn
+
+///     DDRx:*Data direction register*
+///         The DDxn bit in the DDRx Register selects the direction of this pin.
+///         DDxn = 1 => Pin x is configured as output.
+///         DDxn = 0 => Pin x is configured as input.
+/// 
+///     PORTx: *Data register*
+///     If PORTxn is written logic one when the pin is
+///         configured as an input pin, the pull-up resistor is activated. To switch the
+///         pull-up resistor off, PORTxn has to be written logic zero or the pin has to be
+///         configured as an output pin. The port pins are tri-stated when reset condition
+///         becomes active, even if no clocks are running.
+/// 
+///         PORTxn is written logic one when the pin is configured as an output pin,
+///         the port pin is driven high (one). If PORTxn is written logic zero when the pin
+///         is configured as an output pin, the port pin is driven low (zero).
+/// 
+///     PINx:*port input pins*
+///         Writing a logic one to PINxn toggles the value of PORTxn, independent on the value of DDRxn.
+///
+/// Section 13.2.1 and 13.2.2 of ATmega2560P datasheet.
 struct Port{
     pin:u8,
     ddr:u8,
@@ -21,6 +51,8 @@ struct Port{
 }
 
 impl Port {
+    /// Returns mutable reference to the `Port` given `PortName`.
+    /// Section 13.4 of ATmega328P datasheet.
     pub unsafe fn new(name: PortName) -> &'static mut Port {
        match name{
            PortName::A=> { &mut *(0x20 as *mut Port) }
@@ -38,9 +70,8 @@ impl Port {
        } 
     }
 
-  
 
-pub fn name(&self)->PortName{
+    pub fn name(&self)->PortName{
     let address = (self as *const Port) as usize;
     match address{
         0x20=> PortName::A,
@@ -59,18 +90,67 @@ pub fn name(&self)->PortName{
     }
 
 }
-}
 
-
-pub struct Pin
-{
-    port: *mut Port,
-    pin: u8,
+    
 }
- 
+pub struct Pin {
+    port:*mut Port,
+    pin:u8,
+}
 pub enum IOMode{
     Input,
     Output,
+}
+
+impl Pin {
+    ///Toggles the value of PORTxn by writing one to PINxn ,independent of the value of DDRxn.
+    pub fn toggle(&mut self) {
+        unsafe {
+             write_volatile(&mut (*self.port).pin, 0x1 << self.pin) 
+         }
+    }
+    ///set the pin to high
+    pub fn high(&mut self) {
+        // Check if pin number is valid.
+        if self.pin >= 8 {
+            return;
+        }
+        unsafe{
+            //reading the value of PORTxn.
+            let p = unsafe {
+                 read_volatile(&mut (*self.port).port) 
+            };
+            // Read the DDRxn register.
+            let mut ddr_value = unsafe {
+                 read_volatile(&mut (*self.port).ddr)
+            };
+            //toggling the value of PORTxn, if it isn't set to high.
+            if p == 0 && ddr_value == (0x1 << self.pin){
+                self.toggle();
+            }
+        }
+    }
+    ///set the pin to low
+    pub fn low(&mut self) {
+        // Check if pin number is valid.
+        if self.pin >= 8 {
+            return;
+        }
+        unsafe{
+            //reading the value of PORTxn.
+            let p = unsafe {
+                 read_volatile(&mut (*self.port).port) 
+            };
+            // Read the DDRxn register.
+            let mut ddr_value = unsafe {
+                 read_volatile(&mut (*self.port).ddr)
+            };
+            //toggling the value of PORTxn, if it isn't set to low.
+            if p != 0 && ddr_value == (0x1 << self.pin) {
+                self.toggle();
+            }
+        }
+    }
 }
 
 
