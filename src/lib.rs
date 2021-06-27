920285fe02af72a65e8cf7cd3d11602d07328871
Devansh Kumar Jha <77242838+dkjha20@users.noreply.github.com>
2021-06-27 14:18:35 +0530

    #18 #17 lib updated
    All the source codes made till now are included. "delay" and "config" will be included as soon as made.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
+//! Rustduino library for Arduino in Rust Language
 #![no_std]
 #![deny(warnings)]
+#![feature(asm)]
+#![feature(llvm_asm)]
+
 
 /// Library for ATmega2560P chip.
 pub mod atmega2560p {
+    
     /// Hardware Abstraction Library (HAL).
     pub mod hal {
+        pub mod power;
+
         pub mod watchdog;
-        pub mod interrupts;
-        pub mod sim;
-        pub mod port;
+
         pub mod sleep_mode;
+
+        pub mod port;
+
+        pub mod interrupt;
     }
+
 }
