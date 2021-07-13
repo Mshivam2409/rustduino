df1b0c4bdd7daf1cc1eb8e502cc209712574bf7d
Devansh <devansh.kujha2003@gmail.com>
2021-07-13 23:59:15 +0530

    #32 code changes

---
+++ src/com/usart.rs
@@ -0,0 +1,38 @@
+//     RustDuino : A generic HAL implementation for Arduino Boards in Rust
+//     Copyright (C) 2021  Devansh Kumar Jha, Indian Institute of Technology Kanpur
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Affero General Public License as published
+//     by the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Affero General Public License for more details.
+//
+//     You should have received a copy of the GNU Affero General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+
+/// Crates which would be used in the implementation.
+use bit_field::BitField;
+use core;
+use volatile::Volatile;
+// use crate atmega2560p::hal::interrupts;
+
+/// This structure contains various registers needed to control usart communication
+/// through ATMEGA2560P device.
+pub struct Usart {
+     
+}
+
+impl usart {
+    pub unsafe fn new() -> &'static mut Usart {
+
+    }
+
+    pub fn disable() {
+        
+    }
+}
