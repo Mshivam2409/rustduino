89d42b1a9b598b316f022562d6f7f5f67dda4e82
vaskhit <akshitv20@iitk.ac.in>
2021-07-13 18:50:22 +0530

    fixed functions

--- src/atmega328p/hal/i2c.rs
+++ src/atmega328p/hal/i2c.rs
@@ -1,6 +1,11 @@
 use bit_field::BitField;
 use core::ptr::{read_volatile, write_volatile};
-use volatile::Volatile;
+// use volatile::Volatile;
+
+use crate::atmega328p::hal::port;
+use crate::delay;
+
+use super::port::Port;
 
 pub struct Twi {
     twbr: u8,
@@ -19,7 +24,63 @@ static TWSTO: usize = 3;
 static TWWC: usize = 4;
 static TWEN: usize = 5;
 static TWIE: usize = 7;
-static START: u8 = 0xF8;
+// static STATUS_MASK: u8 = 0xF8;
+
+
+// TWSR values (not bits)
+// (taken from avr-libc twi.h)
+// Master
+static START:u8 =					0x08;
+static REP_START:u8 =				0x10;
+// Master Transmitter;
+static MT_SLA_ACK:u8 =				0x18;
+static MT_SLA_NACK:u8 =				0x20;
+static MT_DATA_ACK:u8 =				0x28;
+static MT_DATA_NACK:u8 =			0x30;
+static MT_ARB_LOST:u8 =				0x38;
+// Master Receiver;
+static MR_ARB_LOST:u8 =				0x38;
+static MR_SLA_ACK:u8 =				0x40;
+static MR_SLA_NACK:u8 =				0x48;
+static MR_DATA_ACK:u8 =				0x50;
+static MR_DATA_NACK:u8 =			0x58;
+// Slave Transmitter;
+static ST_SLA_ACK:u8 =				0xA8;
+static ST_ARB_LOST_SLA_ACK:u8 =		0xB0;
+static ST_DATA_ACK:u8 =				0xB8;
+static ST_DATA_NACK:u8 =			0xC0;
+static ST_LAST_DATA:u8 =			0xC8;
+// Slave Receiver;
+static SR_SLA_ACK:u8 =				0x60;
+static SR_ARB_LOST_SLA_ACK:u8 =		0x68;
+static SR_GCALL_ACK:u8 =			0x70;
+static SR_ARB_LOST_GCALL_ACK:u8 =	0x78;
+static SR_DATA_ACK:u8 =				0x80;
+static SR_DATA_NACK:u8 =			0x88;
+static SR_GCALL_DATA_ACK:u8 =		0x90;
+static SR_GCALL_DATA_NACK:u8 =		0x98;
+static SR_STOP:u8 =					0xA0;
+// Misc
+static NO_INFO:u8 =					0xF8;
+static BUS_ERROR:u8 =				0x00;
+
+// defines and constants;
+static TWCR_CMD_MASK: u8 =          0x0F;
+static TWSR_STATUS_MASK: u8 =       0xF8;
+
+// return values;
+static I2C_OK:u8 = 0x00;
+static I2C_ERROR_NODEV: u8 = 0x01;
+
+// static sbi(var, mask)   ((var) |= (uint8_t)(1 << mask))
+// static cbi(var, mask)   ((var) &= (uint8_t)~(1 << mask))
+
+pub fn WRITE_sda() {
+    let mut port =   Port::new(port::PortName::C);  //SDA must be output when writing
+    
+} 
+// pub fn READ_sda()  DDRC = DDRC & 0b11101111 //SDA must be input when reading - don't forget the resistor on SDA!!
+
 
 impl Twi {
     pub fn new() -> &'static mut Self {
@@ -27,25 +88,42 @@ impl Twi {
     }
 
     pub fn init(&mut self) {
+        let mut i:u32 = 0;
         unsafe {
-            write_volatile(&mut self.twcr, 0xA4);
+            write_volatile(&mut self.twcr, 0xA4); // TWINT TWSTA and TWA set to 1
         }
-        while !self.twcr.get_bit(TWINT) {}
+        while !self.twcr.get_bit(TWINT) || i <= 100 {  // waiting for TWINT to be set
+            i += 1;
+        }
 
-        if self.twsr & START != START {
-            panic!("Error");
+        if self.twsr & TWSR_STATUS_MASK != START  { //if status id ok return else panic
+            if i >= 100 {
+                panic!("Timeout");
+            } else {
+                panic!("Error");
+            }
+            
         }
     }
 
     pub fn set_address(&mut self, addr: u8) {
+        let mut i:u32 = 0;
         unsafe {
-            write_volatile(&mut self.twdr, addr);
-            self.twdr.set_bit(TWINT, true);
+            write_volatile(&mut self.twdr, addr); // loading SLA_W to TWDR
+            self.twdr.set_bit(TWINT, true); 
             self.twdr.set_bit(TWEN, true);
         }
-        while !self.twcr.get_bit(TWINT) {}
-        //what is the start condition... finding that
-        // also how to know that acknowledgement is recieved.
+        while !self.twcr.get_bit(TWINT) || i <= 100 { //waiting for TWINT to be set
+            i += 1;
+        } 
+        
+        if self.twsr & TWSR_STATUS_MASK != MT_SLA_ACK { // if ACK recieved.. ok.. else panic
+            if i >= 100 {
+                panic!("Timeout");
+            } else {
+                panic!("Error");
+            }
+        }
     }
 
     pub fn read_from(&mut self,data:u8){
@@ -57,18 +135,10 @@ impl Twi {
         while !self.twcr.get_bit(TWINT) {}
     }
 
-    pub fn read_from(&mut self,data:u8){
-        unsafe {
-            write_volatile(&mut self.twdr,data);
-            self.twdr.set_bit(TWINT, true);
-            self.twdr.set_bit(TWEN, true);
-        }
-        while !self.twcr.get_bit(TWINT) {}
-    }
-
+    
     pub fn stop(&mut self){
-        self.twdr.set_bit(TWINT, true);
-        self.twdr.set_bit(TWEN, true);
-        self.twdr.set_bit(TWSTO, true);
+        unsafe {
+            write_volatile(&mut self.twcr, 0xB0);
+        } 
     }
 }
