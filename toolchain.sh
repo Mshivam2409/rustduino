2517cb2216a14945236c11297f321e5d9b817036
Shivam Malhotra <53910337+Mshivam2409@users.noreply.github.com>
2021-07-13 12:20:03 +0530

    Update toolchain.sh

--- toolchain.sh
+++ toolchain.sh
@@ -1,3 +1,3 @@
 rustup toolchain install nightly-2021-01-07
 rustup override set nightly-2021-01-07
-rustup +nightly-2021-01-07 component add rust-src
+rustup --toolchain nightly-2021-01-07 component add rust-src
