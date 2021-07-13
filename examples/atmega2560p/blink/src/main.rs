c9f2e487f044343f93c11e38cef1bb0e7de7873a
Devansh <devansh.kujha2003@gmail.com>
2021-07-13 13:24:55 +0530

    changes

--- examples/atmega2560p/blink/src/main.rs
+++ examples/atmega2560p/blink/src/main.rs
@@ -19,9 +19,8 @@
 #![deny(warnings)]
 
 /// Crates included which are to be used for the Blinking LED example.
-// use rustduino::atmega2560p::hal;
-use rustduino::atmega2560p::hal::watchdog::WatchDog;
 use rustduino::atmega2560p::hal::pin::Pins;
+use rustduino::atmega2560p::hal::watchdog::WatchDog;
 
 #[no_mangle]
 pub extern "C" fn main() {
