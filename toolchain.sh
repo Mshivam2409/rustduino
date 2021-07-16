23b0bcc914ebd49ea05badbe72e14d3f8164796b
Shivam Malhotra <53910337+Mshivam2409@users.noreply.github.com>
2021-07-16 11:57:26 +0530

    Revert "Nop"

--- toolchain.sh
+++ toolchain.sh
@@ -1,4 +1,3 @@
 rustup toolchain install nightly-2021-01-07
 rustup override set nightly-2021-01-07
-rustup +nightly-2021-01-07 component add rustfmt-preview
 rustup +nightly-2021-01-07 component add rust-src
