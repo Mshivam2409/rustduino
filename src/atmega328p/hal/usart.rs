a1dc4ff01319304868d39d78307975d6a425c265
richa <richa20@iitk.ac.in>
2021-07-17 15:14:14 +0530

    #29 added enums definition

--- src/atmega328p/hal/usart.rs
+++ src/atmega328p/hal/usart.rs
@@ -11,3 +11,57 @@ use crate::rustduino::atmega328p::hal::interrupts;
 use crate::rustduino::atmega328p::hal::port;
 use crate::rustduino::atmega328p::hal::power;
 use crate::delay::{delay_s,delay_ms,delay_us};
+
+
+
+
+/// Selection of which USART is to be used.
+#[derive(Clone, Copy)]
+pub enum UsartNum {
+    usart0,
+}
+
+
+/// Selection of synchronous or asynchronous modes for USART.
+#[derive(Clone, Copy)]
+pub enum UsartModes {
+    norm_async,
+    dou_async,
+    master_sync,
+    slave_sync,
+}
+
+
+/// Selection of the parity mode for USART.
+#[derive(Clone, Copy)]
+pub enum UsartParity {
+    no,
+    even,
+    odd,
+}
+
+/// Selection of the Amount of Data Bits to be transferred or recieved through USART.
+#[derive(Clone, Copy)]
+pub enum UsartDataSize {
+    five,
+    six,
+    seven,
+    eight,
+    nine,
+}
+
+/// Selection of number of stop bits for USART data.
+#[derive(Clone, Copy)]
+pub enum UsartStop {
+    one,
+    two,
+}
+
+/// Selection of the clock parity mode.
+#[derive(Clone, Copy)]
+pub enum UsartPolarity {
+    output_rise,
+    input_rise,
+}
+
+
