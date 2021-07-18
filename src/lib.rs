0ab5b4ee0de4ec5b903be34d9842e442581ec584
Mshivam2409 <shivammalhotra08@gmail.com>
2021-07-19 02:27:36 +0530

    Fix Features

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,11 @@
 // #![allow(non_camel_case_types)]
 
 /// Library for ATmega2560P chip.
-// #[cfg(feature = "atmega2560p")]
+#[cfg(feature = "atmega2560p")]
 pub mod atmega2560p {
 
     /// Hardware Abstraction Library (HAL).
-    // #[cfg(feature = "atmega2560p-hal")]
+    #[cfg(feature = "atmega2560p-hal")]
     pub mod hal {
         pub mod power;
 
@@ -27,11 +27,13 @@ pub mod atmega2560p {
     }
 }
 
-// #[cfg(feature = "atmega2560p")]
+#[cfg(feature = "atmega2560p")]
 pub use atmega2560p::*;
 
+#[cfg(feature = "atmega328p")]
 pub mod atmega328p {
-
+    // Hardware Abstraction Library (HAL).
+    #[cfg(feature = "atmega328p-hal")]
     pub mod hal {
         pub mod port;
 
@@ -46,11 +48,16 @@ pub mod atmega328p {
         pub mod gating;
     }
 
+    #[cfg(feature = "com")]
     pub mod com {
         pub mod i2c;
     }
 }
 
+#[cfg(feature = "atmega328p")]
+pub use atmega328p::*;
+
+#[cfg(feature = "sensors")]
 pub mod sensors {
     pub mod aht10;
 }
