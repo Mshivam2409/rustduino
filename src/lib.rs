6068165e1e35dc7f6a7c8c17c632867711adba5a
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2021-07-19 11:37:54 +0530

    Bug Fixes

--- src/lib.rs
+++ src/lib.rs
@@ -6,32 +6,11 @@
 #![allow(unused_variables)]
 
 /// Library for ATmega2560P chip.
-#[cfg(feature = "atmega2560p")]
+//#[cfg(feature = "atmega2560p")]
 pub mod atmega2560p {
 
     /// Hardware Abstraction Library (HAL).
-    #[cfg(feature = "atmega2560p-hal")]
-    pub mod hal {
-        pub mod power;
-
-        pub mod watchdog;
-
-        pub mod sleep_mode;
-
-        pub mod port;
-
-        pub mod interrupts;
-
-        pub mod pin;
-    }
-}
-
-#[cfg(feature = "atmega2560p")]
-pub use atmega2560p::*;
-
-/// Library for ATmega2560P chip.
-pub mod atmega2560p {
-    /// Hardware Abstraction Library (HAL).
+    // #[cfg(feature = "atmega2560p-hal")]
     pub mod hal {
         pub mod power;
 
@@ -49,19 +28,21 @@ pub mod atmega2560p {
     pub mod com {
         pub mod i2c;
     }
-    //pub mod usart;
 }
-
-#[cfg(feature = "sensors")]
+// #[cfg(feature = "sensors")]
 pub mod sensors {
     pub mod mpu6050;
 }
 
+//#[cfg(feature = "atmega2560p")]
+pub use atmega2560p::*;
+
+
 /// Library for ATmega328P chip.
-#[cfg(feature = "atmega328p")]
+//#[cfg(feature = "atmega328p")]
 pub mod atmega328p {
     /// Hardware Abstraction Library (HAL).
-    #[cfg(feature = "atmega328p-hal")]
+    //#[cfg(feature = "atmega328p-hal")]
     pub mod hal {
         pub mod port;
 
@@ -77,8 +58,8 @@ pub mod atmega328p {
     }
 }
 
-#[cfg(feature = "atmega328p")]
+//#[cfg(feature = "atmega328p")]
 pub use atmega328p::*;
-
+pub mod avr;
 pub mod config;
 pub mod delay;
