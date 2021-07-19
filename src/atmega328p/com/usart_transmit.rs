54dfc1060f56e87014e17472ef30b5f3fa37afed
Kshitij <kshitijk20@iitk.ac.in>
2021-07-20 02:01:15 +0530

    #29; Changes to the file

--- src/atmega328p/com/usart_transmit.rs
+++ src/atmega328p/com/usart_transmit.rs
@@ -24,6 +24,7 @@ use crate::delay::delay_ms;
 /// Crates which would be used in the implementation.
 /// We will be using standard volatile and bit_field crates now for a better read and write.
 use bit_field::BitField;
+use core::{f64,u8,usize};
 use fixed_slice_vec::FixedSliceVec;
 //This is a implementation for Usart
 impl Usart {
@@ -53,7 +54,7 @@ impl Usart {
                 }
             }
 
-            let udr = self.udr.read();
+            let mut udr = self.udr.read();
 
             // If the frame is ready for transmission then the appropriate place is written.
             match len {
@@ -125,23 +126,20 @@ impl Usart {
             }
         }
 
-        unsafe {
             self.ucsrb.update(|srb| {
                 srb.set_bit(3, false);
             });
         }
-    }
 
       /// This function sends a character byte of 5,6,7 or 8 bits
       pub fn transmit_data(&self, data: u8) {
-        unsafe {
-            let ucsra = self.ucsra.read();
-            let udre = ucsra.get_bit(5);
+            let mut ucsra = self.ucsra.read();
+            let mut udre = ucsra.get_bit(5);
 
             let mut i: i32 = 100;
             while udre == false {
-                let ucsra = self.ucsra.read();
-                let udre = ucsra.get_bit(5);
+                 ucsra = self.ucsra.read();
+                 udre = ucsra.get_bit(5);
 
                 if i != 0 {
                     delay_ms(1000);
@@ -156,43 +154,99 @@ impl Usart {
     }
 
      /// This function send data type of string byte by byte.
-     pub fn write(&mut self, data: &mut str) {
-        self.Transmit_enable();
-        let mut vec: FixedSliceVec<u8> = unsafe { FixedSliceVec::from_bytes((data).as_bytes()) };
+     /// This function send data type of string byte by byte.
+     pub fn write_string(&mut self, data: &'static str) {
+        let mut vec: FixedSliceVec<u8> = FixedSliceVec::new(&mut []);
+
+        for c in data.chars() {
+            vec.push(c as u8);
+        }
+
         for i in 0..(vec.len()) {
             self.transmit_data(vec[i]);
         }
-        self.transmit_disable();
     }
 
-   /// This function send data type of int(u32) byte by byte.
-   pub fn write_integer(&mut self, data: u32) {
-    let mut vec: FixedSliceVec<u8> = FixedSliceVec::new(&mut []);
-    let mut a = data;
-    while a != 0 {
-        let rem = a % 10;
-        a = a / 10;
-        match rem {
-            0 => vec.push('0' as u8),
-            1 => vec.push('1' as u8),
-            2 => vec.push('2' as u8),
-            3 => vec.push('3' as u8),
-            4 => vec.push('4' as u8),
-            5 => vec.push('5' as u8),
-            6 => vec.push('6' as u8),
-            7 => vec.push('7' as u8),
-            8 => vec.push('8' as u8),
-            9 => vec.push('9' as u8),
-            _ => (),
+      /// This function send data type of int(u32) byte by byte.
+      pub fn write_integer(&mut self, data: u32) {
+        let mut vec: FixedSliceVec<u8> = FixedSliceVec::new(&mut []);
+        let mut a = data;
+        while a != 0 {
+            let rem = a % 10;
+            a = a / 10;
+            match rem {
+                0 => vec.push('0' as u8),
+                1 => vec.push('1' as u8),
+                2 => vec.push('2' as u8),
+                3 => vec.push('3' as u8),
+                4 => vec.push('4' as u8),
+                5 => vec.push('5' as u8),
+                6 => vec.push('6' as u8),
+                7 => vec.push('7' as u8),
+                8 => vec.push('8' as u8),
+                9 => vec.push('9' as u8),
+                _ => unreachable!(),
+            }
+        }
+        for i in 0..(vec.len()) {
+            self.transmit_data(vec[vec.len() - 1 - i]);
         }
     }
-    for i in 0..(vec.len()) {
-        self.transmit_data(vec[(vec.len) - 1 - i]);
+
+    /// This function send data type of float(f32) byte by byte.
+    pub fn write_float(&mut self, data: f64, precision: u32) {
+        let mut vec: FixedSliceVec<u8> = FixedSliceVec::new(&mut []);
+        let a: f64 = data;
+        let mut f: f64 = a % 1.0;
+        let mut i: i64 = (a - (a % 1.0)) as i64;
+        let mut x: u32 = precision;
+        let mut n: usize = 0;
+        while f != 0.00 && x != 0 {
+            let k: i64 = ((f * 10.0) - ((f * 10.0) % 1.0)) as i64; // gives you decimal digit of data one by one from left to right
+            match k {
+                0 => vec.push('0' as u8),
+                1 => vec.push('1' as u8),
+                2 => vec.push('2' as u8),
+                3 => vec.push('3' as u8),
+                4 => vec.push('4' as u8),
+                5 => vec.push('5' as u8),
+                6 => vec.push('6' as u8),
+                7 => vec.push('7' as u8),
+                8 => vec.push('8' as u8),
+                9 => vec.push('9' as u8),
+                _ => unreachable!(),
+            }
+            f = (f * 10.0) % 1.0; // then f loses its left most digit (in decimal part)
+            x = x - 1;
+            n = n + 1;
+        }
+
+        vec.push('.' as u8);
+
+        while i != 0 {
+            let rem = i % 10;
+            i = i / 10;
+            match rem {
+                0 => vec.push('0' as u8),
+                1 => vec.push('1' as u8),
+                2 => vec.push('2' as u8),
+                3 => vec.push('3' as u8),
+                4 => vec.push('4' as u8),
+                5 => vec.push('5' as u8),
+                6 => vec.push('6' as u8),
+                7 => vec.push('7' as u8),
+                8 => vec.push('8' as u8),
+                9 => vec.push('9' as u8),
+                _ => (),
+            }
+        }
+
+        for ia in 0..(vec.len() - n - 1) {
+            self.transmit_data(vec[vec.len() - 1 - ia]);
+        }
+
+        for ia in 0..n - 1 {
+            vec.push(vec[ia]);
+        }
     }
-}
-/*
-/// This function send data type of float(f32) byte by byte.
-pub fn write_float(&mut self,data : Cf32) {
-}
-*/
 }
