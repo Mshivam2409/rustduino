a5e235f0955afd8503acde68dd7aa0d84c57a2fb
richa <richa20@iitk.ac.in>
2021-07-19 13:37:00 +0530

    #29; enable receive

--- src/atmega328p/hal/usart_receive.rs
+++ src/atmega328p/hal/usart_receive.rs
@@ -8,3 +8,14 @@ use crate::atmega328p::hal::usart_initialize::Usart;
 use crate::delay::delay_ms;
 use bit_field::BitField;
 use core::u32;
+
+impl Usart 
+{
+    /// This function enables the reciever function of microcontroller, whithout enabling it no communication is possible.
+    pub fn recieve_enable(&mut self) {
+        unsafe {
+            self.ucsrb.update(|ucsrb| {
+                ucsrb.set_bit(4, true);
+            });
+        }
+    }
