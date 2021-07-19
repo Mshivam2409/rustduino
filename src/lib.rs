3ee2fbe7c76b15aac69a83dda44a7cf83a6cde28
Kshitij <kshitijk20@iitk.ac.in>
2021-07-19 16:42:12 +0530

    #29; added com folder

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,33 @@
-#![no_std]
-#![deny(warnings)]
-#![feature(asm)]
-#![feature(llvm_asm)]
-
-/// Library for ATmega2560P chip.
-#[cfg(feature = "atmega2560p")]
-pub mod atmega2560p {
-
-    /// Hardware Abstraction Library (HAL).
-    #[cfg(feature = "atmega2560p-hal")]
-    pub mod hal {
-        pub mod power;
-
-        pub mod watchdog;
-
-        pub mod sleep_mode;
-
-        pub mod port;
-
-        pub mod interrupts;
-
-        pub mod pin;
-    }
-}
-
-#[cfg(feature = "atmega2560p")]
-pub use atmega2560p::*;
-
 /// Library for ATmega328P chip.
-#[cfg(feature = "atmega328p")]
+// #[cfg(feature = "atmega328p")]
 pub mod atmega328p {
     /// Hardware Abstraction Library (HAL).
-    #[cfg(feature = "atmega328p-hal")]
+    // #[cfg(feature = "atmega328p-hal")]
     pub mod hal {
-        pub mod gating;
-
-        pub mod interrupt;
+        pub mod port;
 
         pub mod pins;
 
-        pub mod port;
+        pub mod watchdog;
+
+        pub mod interrupt;
 
         pub mod power;
 
-        pub mod usart;
+        pub mod gating;
+    }
 
-        pub mod watchdog;
+    // #[cfg(feature = "com")]
+    pub mod com {
+         pub mod usart;
+
+         pub mod usart_initialize;
+
+         pub mod usart_recieve;
+
+         pub mod usart_transmit;
     }
 }
 
-#[cfg(feature = "atmega328p")]
+// #[cfg(feature = "atmega328p")]
 pub use atmega328p::*;
-
-pub mod avr;
-pub mod config;
-pub mod delay;
