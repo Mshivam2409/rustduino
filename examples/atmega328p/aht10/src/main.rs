338fa1bacfe16077a100948d0fe19d3df1ddfb1e
vaskhit <akshitv20@iitk.ac.in>
2021-07-20 21:29:42 +0530

    added comments

--- examples/atmega328p/aht10/src/main.rs
+++ examples/atmega328p/aht10/src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 #![deny(warnings)]
 
+// Crates included which are to be used for the AHT10 example.
 use rustduino::sensors::*;
 
 #[no_mangle]
@@ -9,12 +10,18 @@ pub fn main() {
     let sensor = AHT10::new(& mut AHT10::get());
 
     loop {
+        // Get relative humidity.
         sensor.relative_humidity();
+
+        // Get temperature
         sensor.temperature();
+
+        // Waiting for 2 seconds.
         rustduino::delay::delay_ms(2000);
     }
-}
+} // Get temperature and relative humidity at 2 second interval
 
+// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
