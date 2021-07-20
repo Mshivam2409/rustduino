d19ebc039d530704ebde9d4ee98098b8a69fb8ff
vaskhit <akshitv20@iitk.ac.in>
2021-07-20 14:47:08 +0530

    #44 removed features

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,14 @@
 #![deny(warnings)]
 #![feature(asm)]
 #![feature(llvm_asm)]
-#![allow(dead_code)]
-#![allow(unused_unsafe)]
-// #![allow(non_camel_case_types)]
+
 
 /// Library for ATmega2560P chip.
-#[cfg(feature = "atmega2560p")]
+// #[cfg(feature = "atmega2560p")]
 pub mod atmega2560p {
 
     /// Hardware Abstraction Library (HAL).
-    #[cfg(feature = "atmega2560p-hal")]
+    // #[cfg(feature = "atmega2560p-hal")]
     pub mod hal {
         pub mod power;
 
@@ -27,18 +25,13 @@ pub mod atmega2560p {
     }
 }
 
-#[cfg(feature = "atmega2560p")]
+// #[cfg(feature = "atmega2560p")]
 pub use atmega2560p::*;
 
-<<<<<<< HEAD
-pub mod atmega328p {
-
-=======
-#[cfg(feature = "atmega328p")]
+// #[cfg(feature = "atmega328p")]
 pub mod atmega328p {
     // Hardware Abstraction Library (HAL).
-    #[cfg(feature = "atmega328p-hal")]
->>>>>>> 0ab5b4ee0de4ec5b903be34d9842e442581ec584
+    // #[cfg(feature = "atmega328p-hal")]
     pub mod hal {
         pub mod port;
 
@@ -53,19 +46,16 @@ pub mod atmega328p {
         pub mod gating;
     }
 
-    #[cfg(feature = "com")]
+    // #[cfg(feature = "com")]
     pub mod com {
         pub mod i2c;
     }
 }
 
-<<<<<<< HEAD
-=======
-#[cfg(feature = "atmega328p")]
+// #[cfg(feature = "atmega328p")]
 pub use atmega328p::*;
 
-#[cfg(feature = "sensors")]
->>>>>>> 0ab5b4ee0de4ec5b903be34d9842e442581ec584
+// #[cfg(feature = "sensors")]
 pub mod sensors {
     pub mod aht10;
 }
