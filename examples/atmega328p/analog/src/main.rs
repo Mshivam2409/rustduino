2991080aadbba8b1ab768000ae12b967fb1b62d3
Devansh <devansh.kujha2003@gmail.com>
2021-07-20 14:00:07 +0530

    #56 build corrections

--- examples/atmega328p/analog/src/main.rs
+++ examples/atmega328p/analog/src/main.rs
@@ -1,15 +1,14 @@
 #![no_std]
 #![no_main]
 #![deny(warnings)]
+#![allow(unused_variables)]
 
-/*/
 /// Crates to be used.
 use rustduino::atmega328p::hal::analog::Analog;
-*/
 
 #[no_mangle]
 pub fn main() {
-    
+    let a = unsafe { Analog::new() };
 }
 
 /// This function is called on panic.
