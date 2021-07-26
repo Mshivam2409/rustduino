c85a40d53ff418efcd6fb24ec353d99eaa3c8073
49sahil <sahilaggarwal9810504971@gmail.com>
2021-07-26 13:42:05 +0530

    #54 Some edits

--- src/avr/display.rs
+++ src/avr/display.rs
@@ -15,7 +15,8 @@
 //     along with this program.  If not, see <https://www.gnu.org/licenses/>
 
 use crate::avr::shift::*;
-use crate::hal::port::*;
+use crate::atmega2560p::hal::port::*;
+//use crate::hal::port::*;
 //use crate::hal::pin::*;
 //use crate::{com, delay::delay_ms};
 
