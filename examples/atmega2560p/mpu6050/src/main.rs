fc1deb04d10bfb52ba170a5354aa5fd4cce26976
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2021-07-28 12:02:12 +0530

    loop initialized

--- examples/atmega2560p/mpu6050/src/main.rs
+++ examples/atmega2560p/mpu6050/src/main.rs
@@ -16,18 +16,20 @@ pub fn main() {
     {
         sensor.begin(MPUdpsT::MPU6050Scale250DPS, MPURangeT::MPU6050Range2G);
     }
-    {
-        let _gyro_output = sensor.read_gyro();
-        //Print these values on screen using USART;
-        //The vec gyro_output stores the raw values of the gyroscope where gyro_output[0] is the x-axis, gyro_output[1] is the y-axis and gyro_output[2] is the z-axis output respectively.These raw values are then converted to degrees per second according to the scale given as input in `begin()` function.
+    loop {
+        {
+            let _gyro_output = sensor.read_gyro();
+            //Print these values on screen using USART;
+            //The vec gyro_output stores the raw values of the gyroscope where gyro_output[0] is the x-axis, gyro_output[1] is the y-axis and gyro_output[2] is the z-axis output respectively.These raw values are then converted to degrees per second according to the scale given as input in `begin()` function.
+        }
+        {
+            let _accel_output = sensor.read_accel();
+            //Print these values on screen using USART;
+            //The vec accel_output stores the raw values of the accelerometer where accel_output[0] is the x-axis, accel_output[1] is the y-axis and accel_output[2] is the z-axis output respectively.These raw values are then converted to g's per second according to the scale given as input in `begin()` function.
+        }
+        // Waiting for 2 seconds.
+        delay_ms(2000);
     }
-    {
-        let _accel_output = sensor.read_accel();
-        //Print these values on screen using USART;
-        //The vec accel_output stores the raw values of the accelerometer where accel_output[0] is the x-axis, accel_output[1] is the y-axis and accel_output[2] is the z-axis output respectively.These raw values are then converted to g's per second according to the scale given as input in `begin()` function.
-    }
-    // Waiting for 2 seconds.
-    delay_ms(2000);
     //we can read the gyroscope and accelerometer values again by running this in loop.
 }
 
